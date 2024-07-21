cddd7084cc2926378ee4e05e512e7f4750235ca7
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-21 19:49:33 -0300

    [ feat ] - Criação da conexão com o banco de dados

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+// Definição do timezone para São Paulo América do Sul.
+date_default_timezone_set('America/Sao_Paulo');
+
+$host = "localhost"; // endereço do servidor
+$database = "LinkNegocios"; // nome do banco de dados
+$usuario = "root"; // usuário do MySQL
+$senha = ""; // senha do MySQL
+
+// Cria a conexão
+try {
+	$connection = new PDO('mysql:host=' . $host . ';dbname=' . $database, $usuario, $senha);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo "Connection error " . $e->getMessage();
+	exit;
+}
+?>
