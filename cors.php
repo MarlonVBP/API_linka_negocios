3df4506908eb35a24d76fe76548c4d8097efd7e5
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-24 00:49:51 -0300

    [ feat ] - Update cors policy

--- cors.php
+++ cors.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 // Define os cabeçalhos para permitir acesso de qualquer origem (*)
 header("Access-Control-Allow-Origin: *");
 // Permite que os métodos HTTP GET, POST, PUT e DELETE sejam usados a partir de origens diferentes
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 // Define os cabeçalhos permitidos para serem incluídos na solicitação
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// Define o tipo de conteúdo como JSON
+header('Content-Type: application/json');
 ?>
