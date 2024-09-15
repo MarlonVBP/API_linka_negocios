b95f40223d7f2ad57d29a84cb6cf850ece80e8b4
MarlonVBP <marlonvicctor13@gmail.com>
2024-09-14 21:21:37 -0300

    #

--- public/servicos/read.php
+++ public/servicos/read.php
@@ -18,6 +18,16 @@ try {
 
     $servicos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    foreach ($servicos as &$servico) {
+        // Decodificar as entidades HTML
+        $servico['titulo'] = html_entity_decode($servico['titulo'], ENT_QUOTES, 'UTF-8');
+        $servico['descricao'] = html_entity_decode($servico['descricao'], ENT_QUOTES, 'UTF-8');
+        $servico['conteudo1'] = html_entity_decode($servico['conteudo1'], ENT_QUOTES, 'UTF-8');
+        $servico['conteudo2'] = html_entity_decode($servico['conteudo2'], ENT_QUOTES, 'UTF-8');
+        $servico['conteudo3'] = html_entity_decode($servico['conteudo3'], ENT_QUOTES, 'UTF-8');
+        
+    }
+
     if ($servicos) {
         http_response_code(200);
         echo json_encode([
