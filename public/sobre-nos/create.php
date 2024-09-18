29b6119cfb285ea2baf6ae5a8c75ac4da851a6bb
masooanjos <masooanjos@gmail.com>
2024-09-18 18:29:52 -0300

    [feat] - reCAPTCHA contato

--- public/sobre-nos/create.php
+++ public/sobre-nos/create.php
@@ -1,9 +1,11 @@
 <?php
 include '../../cors.php';
 include '../../conn.php';
+include '../../variaveis_globais/secretKey.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Verificar se o método é POST
 if ($method === 'OPTIONS') {
     exit;
 }
@@ -17,9 +19,44 @@ if ($method !== 'POST') {
     exit;
 }
 
+// Obter os dados JSON do corpo da solicitação
 $data = json_decode(file_get_contents("php://input"));
 
+if (!$data) {
+    http_response_code(400);
+    echo json_encode([
+        'success' => 0,
+        'message' => 'Dados inválidos. Verifique o corpo da solicitação.',
+    ]);
+    exit;
+}
+
 try {
+    // Validar o token do reCAPTCHA
+    $token = $data->recaptcha;
+    if (empty($token)) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => 0,
+            'message' => 'Token reCAPTCHA não fornecido.'
+        ]);
+        exit;
+    }
+
+    // Verificar o token do reCAPTCHA com o Google
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$token}");
+    $result = json_decode($response);
+
+    if (!$result->success || $result->score < 0.5) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => 0,
+            'message' => 'Erro de captcha, por favor tentar novamente...'
+        ]);
+        exit;
+    }
+
+    // Sanitizar os dados recebidos
     $nome = htmlspecialchars(trim($data->nome));
     $email = htmlspecialchars(trim($data->email));
     $telefone = htmlspecialchars(trim($data->telefone));
@@ -27,6 +64,26 @@ try {
     $area_atuacao = htmlspecialchars(trim($data->area_atuacao));
     $mensagem = htmlspecialchars(trim($data->mensagem));
 
+    // Validar os campos obrigatórios
+    if (empty($nome) || empty($email) || empty($telefone) || empty($mensagem)) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => 0,
+            'message' => 'Todos os campos são obrigatórios.',
+        ]);
+        exit;
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => 0,
+            'message' => 'Formato de e-mail inválido.',
+        ]);
+        exit;
+    }
+
+    // Preparar a consulta SQL para inserção
     $query = "INSERT INTO `contato` (
             nome,
             email,
@@ -46,6 +103,7 @@ try {
 
     $stmt = $connection->prepare($query);
 
+    // Associar os valores aos parâmetros da consulta
     $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
     $stmt->bindValue(':email', $email, PDO::PARAM_STR);
     $stmt->bindValue(':telefone', $telefone, PDO::PARAM_STR);
@@ -53,8 +111,9 @@ try {
     $stmt->bindValue(':area_atuacao', $area_atuacao, PDO::PARAM_STR);
     $stmt->bindValue(':mensagem', $mensagem, PDO::PARAM_STR);
 
+    // Executar a consulta e verificar se a inserção foi bem-sucedida
     if ($stmt->execute()) {
-        http_response_code(201); 
+        http_response_code(201);
         echo json_encode([
             'success' => 1,
             'message' => 'Dados inseridos com sucesso'
@@ -62,6 +121,8 @@ try {
         exit;
     }
 
+    // Se a inserção falhar, retornar uma mensagem de erro
+    http_response_code(500);
     echo json_encode([
         'success' => 0,
         'message' => 'Falha na inserção dos dados'
@@ -69,6 +130,7 @@ try {
     exit;
 
 } catch (PDOException $e) {
+    // Definir código de resposta HTTP para erro interno do servidor
     http_response_code(500);
     echo json_encode([
         'success' => 0,
