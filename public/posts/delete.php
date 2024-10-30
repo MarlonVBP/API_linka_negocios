1bbae913847265a0020c01b893ee6973a41d6125
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-29 21:37:34 -0300

    #

--- public/posts/delete.php
+++ public/posts/delete.php
@@ -9,27 +9,16 @@ $response = [
 
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
     $data = json_decode(file_get_contents("php://input"), true);
-    
-    if (isset($data['email'])) {
-        $email = $data['email'];
 
-        $sql = "SELECT id FROM admin WHERE email=:email";
-        $stmt = $connection->prepare($sql);
-        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-        $stmt->execute();
+    if (isset($data['id']) && is_numeric($data['id'])) {
+        $post_id = intval($data['id']);
 
-        if ($stmt->rowCount() > 0) {
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            $usuario_id = $user['id'];
-
-            if (isset($data['id'])) {
-                $post_id = intval($data['id']);
-
-                $query = "DELETE FROM postagens WHERE id = :post_id AND usuario_id = :usuario_id";
+        try {
+            if ($connection) {
+                $query = "DELETE FROM postagens WHERE id = :post_id";
                 $stmt = $connection->prepare($query);
 
                 $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
-                $stmt->bindValue(':usuario_id', $usuario_id, PDO::PARAM_INT);
 
                 if ($stmt->execute()) {
                     $response = [
@@ -37,31 +26,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
                         'message' => 'Postagem excluída com sucesso'
                     ];
                 } else {
-                    $response = [
-                        'success' => false,
-                        'message' => 'Falha na exclusão da postagem'
-                    ];
+                    $response['message'] = 'Falha na exclusão da postagem';
                 }
             } else {
-                $response = [
-                    'success' => false,
-                    'message' => 'ID da postagem não fornecido'
-                ];
+                $response['message'] = 'Falha na conexão com o banco de dados';
             }
-        } else {
-            $response = [
-                'success' => false,
-                'message' => 'Usuário não encontrado'
-            ];
+        } catch (PDOException $e) {
+            $response['message'] = 'Erro de banco de dados: ' . $e->getMessage();
         }
     } else {
-        $response = [
-            'success' => false,
-            'message' => 'Email não fornecido'
-        ];
+        $response['message'] = 'ID da postagem não fornecido ou inválido';
     }
 }
 
 header('Content-Type: application/json');
 echo json_encode($response);
-?>
