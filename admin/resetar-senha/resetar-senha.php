ddb8899d7577246cdabb6f064dab0fc3d8f24413
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-30 22:37:38 -0300

    :rocket: feat: enviando email de redifinição de senha

--- admin/resetar-senha/resetar-senha.php
+++ admin/resetar-senha/resetar-senha.php
@@ -6,24 +6,24 @@ include '../../conn.php';
 $data = json_decode(file_get_contents('php://input'));
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
- $token = $data->token;
- $new_password = password_hash($data->new_password, PASSWORD_DEFAULT);
+    $token = $data->token;
+    $new_password = password_hash($data->new_password, PASSWORD_BCRYPT);
 
- // Verificar se o token é válido e não expirou
- $query = "SELECT id FROM admin WHERE reset_token = ? AND reset_expires > NOW()";
- $stmt = $connection->prepare($query);
- $stmt->bindValue("s", $token);
- $stmt->execute();
+    $query = "SELECT id, reset_expires FROM admin WHERE reset_token = ? AND reset_expires > NOW()";
+    $stmt = $connection->prepare($query);
+    $stmt->bindValue(1, $token);
+    $stmt->execute();
 
- if ($stmt->rowCount() > 0) {
-  // Redefinir a senha
-  $query = "UPDATE admin SET senha = ?, reset_token = NULL, reset_expires = NULL WHERE reset_token = ?";
-  $stmt = $connection->prepare($query);
-  $stmt->bindValue("ss", $new_password, $token);
-  $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        // Redefinir a senha
+        $query = "UPDATE admin SET senha = ?, reset_token = NULL, reset_expires = NULL WHERE reset_token = ?";
+        $stmt = $connection->prepare($query);
+        $stmt->bindValue(1, $new_password);
+        $stmt->bindValue(2, $token);
+        $stmt->execute();
 
-  echo json_encode(["message" => "Senha redefinida com sucesso!"]);
- } else {
-  echo json_encode(["error" => "Token inválido ou expirado!"]);
- }
+        echo json_encode(["message" => "Senha redefinida com sucesso!"]);
+    } else {
+        echo json_encode(["error" => "Token inválido ou expirado!"]);
+    }
 }
